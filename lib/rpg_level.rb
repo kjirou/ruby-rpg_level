ddf9ebebc6bd1eed7e693483823a9f1e3af4eac6
kjirou <kjirou.web@gmail.com>
2016-09-14 02:36:48 +0900

    Add a method

--- lib/rpg_level.rb
+++ lib/rpg_level.rb
@@ -1,4 +1,33 @@
-require "rpg_level/version"
+require 'rpg_level/version'
+
 
 class RpgLevel
+  def initialize
+    @min_level = 1
+  end
+
+  private
+
+  # TODO: How to define block requirement?
+  def generate_necessary_exps(start_level: 1, max_level: 99)
+    exps = []
+    memo = {}
+
+    (@min_level..max_level).map do |level|
+      exp = if level <= start_level then
+        0
+      else
+        yield({
+          level: level,
+          min_level: @min_level,
+          start_level: start_level,
+          max_level: max_level,
+          exps: exps,
+          memo: memo
+        })
+      end
+      exps << exp
+      exp
+    end
+  end
 end
--- spec/rpg_level_spec.rb
+++ spec/rpg_level_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
 describe RpgLevel do
+  let (:rpg_level) {
+    RpgLevel.new
+  }
+
   it 'has a version number' do
     expect(RpgLevel::VERSION).not_to be nil
+  end
+
+  it '#initialize' do
+    RpgLevel.new
+  end
+
+  describe '#generate_necessary_exps' do
+    it 'default args' do
+      # TODO: How to destruct the `data` hash?
+      necessary_exps = rpg_level.send(:generate_necessary_exps) {|data| data[:level]}
+      expect(necessary_exps.size).to be 99
+      expect(necessary_exps.inject(:+)).to be((2..99).inject(:+))
+    end
   end
 end
