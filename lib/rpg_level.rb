96bd45e59a5346a4c85649381587253b7c02171f
kjirou <kjirou.web@gmail.com>
2016-09-17 23:35:04 +0900

    Change the meaning of @min_level
    Before:
    max_level = necessary_exps.length
    
    After:
    max_level = @min_level + necessary_exps.length

--- lib/rpg_level.rb
+++ lib/rpg_level.rb
@@ -6,37 +6,36 @@ class RpgLevel
 
   def initialize
     @min_level = 1
+
+    # Necessary exps from the MIN_LEVEL
     @necessary_exps = []
   end
 
-  def define_exp_table(start_level: 1, max_level: 99)
-    raise ArgumentError unless block_given?
-    @necessary_exps = generate_necessary_exps(start_level, max_level) {|data| yield(data)}
-  end
+  #def define_exp_table(start_level: 1, max_level: 99)
+  #  raise ArgumentError unless block_given?
+  #  @necessary_exps = generate_necessary_exps(start_level, max_level) {|data| yield(data)}
+  #end
 
   def max_level
-    @necessary_exps.length
+    @min_level + @necessary_exps.length
   end
 
   private
 
-  def generate_necessary_exps(start_level, max_level)
+  def generate_necessary_exps(max_level)
+    raise ArgumentError 'max_level less than min_level' if max_level < @min_level
+
     exps = []
     memo = {}
 
-    (@min_level..max_level).map do |level|
-      exp = if level <= start_level then
-        0
-      else
-        yield({
-          level: level,
-          min_level: @min_level,
-          start_level: start_level,
-          max_level: max_level,
-          exps: exps,
-          memo: memo
-        })
-      end
+    ((@min_level + 1)..max_level).map do |level|
+      exp = yield({
+        level: level,
+        min_level: @min_level,
+        max_level: max_level,
+        exps: exps,
+        memo: memo
+      })
       exps << exp
       exp
     end
--- spec/rpg_level_spec.rb
+++ spec/rpg_level_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pry'
 
 describe RpgLevel do
   it 'has a version number' do
@@ -14,87 +15,87 @@ describe RpgLevel do
       RpgLevel.new
     }
 
-    it 'can specify start_level and max_level' do
-      necessary_exps = rpg_level.send(:generate_necessary_exps, 2, 5) {|level:, **rest| level}
-      expect(necessary_exps.size).to be 5
-      expect(necessary_exps.inject(:+)).to be(3 + 4 + 5)
+    it 'can specify the max_level' do
+      necessary_exps = rpg_level.send(:generate_necessary_exps, 3) {|level:, **rest| level}
+      expect(necessary_exps.length).to be 2
+      expect(necessary_exps.inject(:+)).to eq(2 + 3)
     end
 
-    it 'can refer :min_level, :start_level and :max_level in the block' do
-      necessary_exps = rpg_level.send(:generate_necessary_exps, 2, 3) do |data|
-        data[:min_level] * data[:start_level] * data[:max_level]
+    it 'can refer the :max_level in the block' do
+      necessary_exps = rpg_level.send(:generate_necessary_exps, 3) do |data|
+        data[:max_level]
       end
-      expect(necessary_exps.last).to be(1 * 2 * 3)
+      expect(necessary_exps.last).to eq 3
     end
 
-    it 'can use :exps in the block' do
-      necessary_exps = rpg_level.send(:generate_necessary_exps, 1, 5) do |level:, exps:, **rest|
-        level - 1 + exps.inject(:+)
+    it 'can use the :exps in the block' do
+      necessary_exps = rpg_level.send(:generate_necessary_exps, 5) do |level:, exps:, **rest|
+        level - 1 + exps.inject(0, :+)
       end
-      expect(necessary_exps[1]).to be(1)
-      expect(necessary_exps[2]).to be(2 + 1)
-      expect(necessary_exps[3]).to be(3 + (2 + 1) + 1)
-      expect(necessary_exps[4]).to be(4 + (3 + (2 + 1) + 1) + (2 + 1) + 1)
+      expect(necessary_exps[0]).to eq(1)
+      expect(necessary_exps[1]).to eq(2 + 1)
+      expect(necessary_exps[2]).to eq(3 + (2 + 1) + 1)
+      expect(necessary_exps[3]).to eq(4 + (3 + (2 + 1) + 1) + (2 + 1) + 1)
     end
 
     it 'can use :memo in the block' do
-      necessary_exps = rpg_level.send(:generate_necessary_exps, 1, 3) do |level:, memo:, **rest|
+      necessary_exps = rpg_level.send(:generate_necessary_exps, 3) do |level:, memo:, **rest|
         if level == 2 then
           memo[:tmp] = 5
         elsif level == 3 then
           memo[:tmp] * 2
         end
       end
-      expect(necessary_exps[1]).to be(5)
-      expect(necessary_exps[2]).to be(5 * 2)
+      expect(necessary_exps[0]).to be(5)
+      expect(necessary_exps[1]).to be(5 * 2)
     end
   end
 
-  describe '#define_exp_table' do
-    let (:rpg_level) {
-      RpgLevel.new
-    }
+  #describe '#define_exp_table' do
+  #  let (:rpg_level) {
+  #    RpgLevel.new
+  #  }
 
-    it 'should raise a ArgumentError unless block is given' do
-      expect {rpg_level.send(:generate_necessary_exps)}.to raise_error ArgumentError
-    end
+  #  it 'should raise a ArgumentError unless block is given' do
+  #    expect {rpg_level.send(:generate_necessary_exps)}.to raise_error ArgumentError
+  #  end
 
-    it 'has default args' do
-      rpg_level.define_exp_table {|level:, **rest| level}
-      expect(rpg_level.necessary_exps.size).to be 99
-      expect(rpg_level.necessary_exps[0]).to be 0
-      expect(rpg_level.necessary_exps[1]).to be 2
-      expect(rpg_level.necessary_exps[98]).to be 99
-    end
+  #  it 'has default args' do
+  #    rpg_level.define_exp_table {|level:, **rest| level}
+  #    expect(rpg_level.necessary_exps.size).to be 99
+  #    expect(rpg_level.necessary_exps[0]).to be 0
+  #    expect(rpg_level.necessary_exps[1]).to be 2
+  #    expect(rpg_level.necessary_exps[98]).to be 99
+  #  end
 
-    it 'can specify :start_level and :max_level' do
-      rpg_level.define_exp_table(start_level: 2, max_level: 4) {|level:, **rest| level}
-      expect(rpg_level.necessary_exps.size).to be 4
-      expect(rpg_level.necessary_exps[1]).to be 0
-      expect(rpg_level.necessary_exps[2]).to be 3
-      expect(rpg_level.necessary_exps[3]).to be 4
-    end
-  end
+  #  it 'can specify :start_level and :max_level' do
+  #    rpg_level.define_exp_table(start_level: 2, max_level: 4) {|level:, **rest| level}
+  #    expect(rpg_level.necessary_exps.size).to be 4
+  #    expect(rpg_level.necessary_exps[1]).to be 0
+  #    expect(rpg_level.necessary_exps[2]).to be 3
+  #    expect(rpg_level.necessary_exps[3]).to be 4
+  #  end
+  #end
 
-  describe '#min_level' do
-    let (:rpg_level) {
-      RpgLevel.new
-    }
+  #describe '#min_level' do
+  #  let (:rpg_level) {
+  #    RpgLevel.new
+  #  }
 
-    it do
-      rpg_level.define_exp_table {|level:, **rest| level}
-      expect(rpg_level.min_level).to eq 1
-    end
-  end
+  #  it 'should be' do
+  #    rpg_level.define_exp_table {|level:, **rest| level}
+  #    expect(rpg_level.min_level).to eq 1
+  #  end
+  #end
 
-  describe '#max_level' do
-    let (:rpg_level) {
-      RpgLevel.new
-    }
+  #describe '#max_level' do
+  #  let (:rpg_level) {
+  #    RpgLevel.new
+  #  }
 
-    it do
-      rpg_level.define_exp_table(max_level: 5) {|level:, **rest| level}
-      expect(rpg_level.max_level).to eq 5
-    end
-  end
+  #  it 'should be' do
+  #    rpg_level.define_exp_table(max_level: 5) {|level:, **rest| level}
+  #    expect(rpg_level.max_level).to eq 5
+  #  end
+  #end
 end
